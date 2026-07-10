4c737845369e88583f8a1a97f40102842a7027f0
sakibulislam75 <sakibulislam1623@gmail.com>
2026-07-10 20:27:37 +0600

    feat: setup better-auth with email/password and Google OAuth, add sign-in/sign-up pages

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   /* config options here */
+   reactCompiler: true,
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: 'i.pinimg.com',
+            pathname: '/**',
+         },
+         {
+            protocol: 'https',
+            hostname: '**',
+         },
+         {
+            protocol: 'http',
+            hostname: '**',
+         },
+      ],
+   },
+};
+
+export default nextConfig;
---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,113 @@
+'use client';
+import { authClient } from '@/lib/auth-client';
+import { Check } from '@gravity-ui/icons';
+import {
+   Button,
+   Card,
+   Description,
+   FieldError,
+   Form,
+   Input,
+   Label,
+   TextField,
+} from '@heroui/react';
+import { router } from 'better-auth/api';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+
+export default function SignUpPage() {
+   const router = useRouter();
+   const onSubmit = async (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const userData = Object.fromEntries(formData.entries());
+
+      const { data, error } = await authClient.signUp.email({
+         name: userData.name, // required
+         email: userData.email, // required
+         password: userData.password, // required
+         image: userData.image,
+      });
+      console.log(data, error);
+      if (error) {
+         toast.error(error.message);
+      } else {
+         toast.success('Successfully signed up');
+         router.push('/');
+      }
+   };
+
+   return (
+      <Card className="border mx-auto w-125 py-10 mt-22">
+         <h1 className="text-center text-2xl font-bold">Sign Up</h1>
+
+         <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
+            <TextField isRequired name="name" type="text">
+               <Label>Name</Label>
+               <Input placeholder="Enter your name" />
+               <FieldError />
+            </TextField>
+
+            <TextField isRequired name="image" type="text">
+               <Label>Image URL</Label>
+               <Input placeholder="Image URL" />
+               <FieldError />
+            </TextField>
+
+            <TextField
+               isRequired
+               name="email"
+               type="email"
+               validate={(value) => {
+                  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                     return 'Please enter a valid email address';
+                  }
+
+                  return null;
+               }}
+            >
+               <Label>Email</Label>
+               <Input placeholder="john@example.com" />
+               <FieldError />
+            </TextField>
+
+            <TextField
+               isRequired
+               minLength={8}
+               name="password"
+               type="password"
+               validate={(value) => {
+                  if (value.length < 8) {
+                     return 'Password must be at least 8 characters';
+                  }
+                  if (!/[A-Z]/.test(value)) {
+                     return 'Password must contain at least one uppercase letter';
+                  }
+                  if (!/[0-9]/.test(value)) {
+                     return 'Password must contain at least one number';
+                  }
+
+                  return null;
+               }}
+            >
+               <Label>Password</Label>
+               <Input placeholder="Enter your password" />
+               <Description>
+                  Must be at least 8 characters with 1 uppercase and 1 number
+               </Description>
+               <FieldError />
+            </TextField>
+
+            <div className="flex gap-2">
+               <Button type="submit">
+                  <Check />
+                  Submit
+               </Button>
+               <Button type="reset" variant="secondary">
+                  Reset
+               </Button>
+            </div>
+         </Form>
+      </Card>
+   );
+}
