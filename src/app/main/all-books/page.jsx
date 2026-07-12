c1476ff4090e48f022b3d10d1b5584818b11ac61
sakibulislam75 <sakibulislam1623@gmail.com>
2026-07-12 20:39:24 +0600

    feat: display books data on all books page

--- src/app/main/all-books/page.jsx
+++ src/app/main/all-books/page.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const AllBooks = () => {
+import { getAllBooks } from '@/lib/data';
+import BookCard from '@/components/BookCard';
+
+const AllBooks = async () => {
+   const allBooks = await getAllBooks();
    return (
-      <div>
-         <h1>All Books</h1>
-      </div>
+      <section className="w-11/12 mx-auto my-12">
+         <h1 className="font-bold text-3xl mb-8 text-gray-900 dark:text-white">All Books</h1>
+
+         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
+            {allBooks.map((book) => (
+               <BookCard key={book.id} book={book} />
+            ))}
+         </div>
+      </section>
    );
 };
 
